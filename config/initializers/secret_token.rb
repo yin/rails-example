b49d8c018da75404b9d94d6ead295329859816eb
Matej (yin) Gagyi <yinotaurus@gmail.com>
2013-02-14 20:22:34 +0100

    Initialized rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuPrime::Application.config.secret_token = '28e3b98b357147e583039771f73c90166238c4f282ea4891c693ed320eddf52bc2a57652b0b38f2dfd59c67ae6c27269317f7273ce2e507af43ca8d4abc8a882'
